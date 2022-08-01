1a396ae3f8df7ce9e048fcb05ed4e55ec11fb360
Alexandre Matis <alexandrematis@gmail.com>
2022-08-01 12:58:06 -0300

    configurações iniciais

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Alexandre Matis ',
+    site_url: 'https://www.alexandrematis.com.br',
+    feed_url: 'https://www.alexandrematis.com.br/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
